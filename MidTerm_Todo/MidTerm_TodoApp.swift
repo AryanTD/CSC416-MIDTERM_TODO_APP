d125610501b15239d769b2b84f25f66191abed19
AryanTD <aryantandon285@gmail.com>
2022-03-22 09:13:21 -0400

    Initial Commit

---
+++ MidTerm_Todo/MidTerm_TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MidTerm_TodoApp.swift
+//  MidTerm_Todo
+//
+//  Created by student on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct MidTerm_TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
